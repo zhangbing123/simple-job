2e7123fd220a68ee85cf361b298ba3826a84a51c
zhangbing <1060570381@qq.com>
2020-10-16 18:29:00 +0800

    增加公网配置

--- src/main/java/com/schedule/simplejob/SimpleJobApplication.java
+++ src/main/java/com/schedule/simplejob/SimpleJobApplication.java
@@ -1,18 +1,18 @@
-//package com.schedule.simplejob;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import tk.mybatis.spring.annotation.MapperScan;
-//
-//@Slf4j
-//@MapperScan(basePackages = {"com.schedule.simplejob.mapper"})
-//@SpringBootApplication
-//public class SimpleJobApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(SimpleJobApplication.class, args);
-//        log.info("server start successfully！！！");
-//    }
-//
-//}
+package com.schedule.simplejob;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@Slf4j
+@MapperScan(basePackages = {"com.schedule.simplejob.mapper"})
+@SpringBootApplication
+public class SimpleJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJobApplication.class, args);
+        log.info("server start successfully！！！");
+    }
+
+}
